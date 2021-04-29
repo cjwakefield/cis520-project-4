b94e178bd4d1af837f5d224a9335c924880011d7
Colin Wakefield <cjwakefield@selene.beocat.ksu.edu>
2021-04-29 15:03:47 -0500

    more data

--- 3way-openmp/runAll.sh
+++ 3way-openmp/runAll.sh
@@ -3,8 +3,10 @@
 for i in 1 2 4 8 16
 do	
 	for k in 1 2 3 4 5 6 7 8 9 10
-	do 
-		sbatch --constraint=elves --ntasks-per-node=$i --nodes=1 --mem=48G --output "outputdata/core${i}openmp${k}.out" run.sh
-
+	do
+		for j in 2 
+		do 
+			sbatch --constraint=elves --ntasks-per-node=${i} --nodes=${j} --mem=48G --output "outputdata/core${i}openmp${k}node${j}.out" run.sh
+		done
 	done
 done
